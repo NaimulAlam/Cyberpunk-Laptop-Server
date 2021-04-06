aee044122111aeb1d11758df76ba201177bf7080
Naimul Alam <naimul2707@gmail.com>
2021-04-06 04:07:41 +0200

    almost done

--- index.js
+++ index.js
@@ -13,26 +13,23 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(bodyParser.json());
 
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("WellCome to Cyberpunk Database");
+});
 
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
-    // console.log('connection err', err);
   const laptopCollection = client.db("cyberpunkLaptop").collection("laptops");
+  const OrderCollection = client.db("cyberpunkLaptop").collection("orders");
 
     app.get('/laptops', (req, res) => {
         laptopCollection.find({})
         .toArray((err, items) => {
-            // console.log('from db', items);
             res.send(items)
         })
     })
 
-    //getting user checkout info......................
     app.get("/laptop/:id", (req, res) => {
       laptopCollection.find({_id: ObjectID(req.params.id) })
         .toArray((err, documents) => {
@@ -47,9 +44,33 @@ client.connect(err => {
         res.send(result.insertedCount > 0)
     })
   })
+  
+  app.post('/addOrders', (req, res) => {
+    const newOrder = req.body;
+    OrderCollection.insertOne(newOrder)
+    .then(result => {
+        res.send(result.insertedCount > 0)
+    })
+  })
 
-//   client.close();
+  app.get('/orders', (req,res) => {
+    OrderCollection.find({email: req.query.email})
+    .toArray((err, documents) => {
+      res.send(documents);
+    })
+  })
+
+  app.delete("/delete/:id", (req, res) => {
+    laptopCollection.deleteOne({_id: ObjectID(req.params.id)})
+    .then((result) =>{
+      res.send(result.deletedCount > 0);
+    })
+  });  
+
+
 });
+
+
 
 
 
